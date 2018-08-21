373c97a7d29ff65e46629ab4a0b843ee2fa94e1d
Zarrass <35363369+Zarrass@users.noreply.github.com>
2018-08-21 15:34:17 +0700

    Homework 4
    Ксожалению я так и не понял, как заставить обновляться секунды онлайн

--- sis-angular/src/app/first/first.component.ts
+++ sis-angular/src/app/first/first.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AboutmeClass} from "./aboutme-class";
+
+
 
 @Component({
   selector: 'app-first',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
+  aboutme:AboutmeClass;
+  dateNY:number = new Date(2019, 0, 1).getTime();
+  dateNow:number = Date.now();
+  dateResult:number = (this.dateNY-this.dateNow);
+  dateLast:Date = new Date(this.dateResult);
+  monthLast:number = this.dateLast.getMonth();
+  dayLast:number = this.dateLast.getDay();
+  hoursLast:number = this.dateLast.getHours();
+  minutesLast:number= this.dateLast.getMinutes();
+  secondsLast:number= this.dateLast.getSeconds();
 
-  constructor() { }
+  private timerId: number;
+
+  constructor() {
+    this.aboutme = new AboutmeClass(
+      'Михаил Дергачев',
+      new Date(1990, 3, 4),
+      'город Краснояск',
+      'неоконченное высшее, СибГАУ, "Физика".')
+  }
 
   ngOnInit() {
+    this.timerId = setInterval(() => this.dateResult--, 1000);
+
+
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.timerId);
   }
 
 }
