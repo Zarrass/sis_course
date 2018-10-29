cf995de0292db0d185764aebd699e944e30734ae
Zarrass <zarrass@mail.ru>
2018-10-29 11:32:37 +0700

    Темы

--- sis-angular/src/app/app.module.ts
+++ sis-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
@@ -14,7 +15,8 @@ import { SkillsComponent } from './skills/skills.component';
     SkillsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
